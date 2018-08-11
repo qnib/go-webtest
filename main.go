513839b221be321304aa813ae34f3b5152a42bd7
Christian Kniep <christian@qnib.org>
2018-08-11 10:29:39 +0200

    Release version 0.1.2

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "Webserver for testing purposes"
 	app.Usage = "webtest [options]"
-	app.Version = "0.1.1"
+	app.Version = "0.1.2"
 	app.Flags = webtest.DefaultFlags()
 	app.Action = webtest.Run
 	app.Run(os.Args)
